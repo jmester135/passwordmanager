c7cc5262a3a50e11f806319d45e2fb0bd377b376
Justin Mester <justinmester@Justins-MBP.fritz.box>
2020-05-17 14:44:56 +0200

    Login Maske

---
+++ Login.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>
+      Passwort Login
+    </title>
+  </head>
+  <body>
+    <form action='phphash.php' method='get'>
+      Email:<br>
+      <input type="text" name="Email">
+      Login Passwort:<br>
+      <input type='password' name='password'>
+  </body>
+</html>
