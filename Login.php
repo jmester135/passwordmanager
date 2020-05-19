13cb70f6e90694477d0388505e4b40db97bec54f
Justin Mester <justinmester@Justins-MBP.fritz.box>
2020-05-19 10:21:42 +0200

    DB_Connection

--- Login.php
+++ Login.php
@@ -1,14 +1,44 @@
+<!DOCTYPE html>
+
 <html>
   <head>
+    <meta charset="utf-8">
     <title>
       Passwort Login
     </title>
   </head>
   <body>
-    <form action='phphash.php' method='get'>
+    <form action="Login.php" method="post">
       Email:<br>
       <input type="text" name="Email">
-      Login Passwort:<br>
-      <input type='password' name='password'>
+      <br> Login Passwort: <br>
+      <input type = "password" name="password">
+      <br>
+      <input type = "submit"> <br>
+
+    <?php
+    session_start();
+    $_SESSION['login']=0;
+    /*
+    //DB Login_Daten
+    $db_server="Serververbindung";
+    $db_user="";
+    $db_password="";
+    $db="";
+    $db_verbindung=mysqli_connect($db_server,$db_user,$db_password,$db) or die ("Verbinduns Error");
+    */
+    $db_password = '098f6bcd4621d373cade4e832627b4f6';  //PW = test
+    $hash_pw = $_POST["password"];
+
+    $hash = md5($hash_pw);
+
+    if($hash == $db_password) {
+      $_SESSION['login']=1;
+      header("Location: http://localhost/dashboard/phphash.php");
+      exit();
+    }else {
+      echo "Fehlerhafte Eingabe";
+    }
+     ?>
   </body>
 </html>
--- phphash.php
+++ phphash.php
@@ -5,27 +5,38 @@
 	Passwort Verschlüsselung
 	</title>
 	</head>
-
 	<body>
+		<?php
+		session_start();
+		$login=$_SESSION['login'];
+		if($login==1){
+			echo '	<form action="phphash.php" method="get">
+					Zu verschlüsselndes Wort: <input type="text" name="cipher">
+					<br>
+					<input type="submit">
+				</form>
+				<br>';
 
-	<form action=”phphash.php” method=”get”>
-		Zu verschlüsselndes Wort: <input type=”text” name=”password”>
-		<input type=”submit”>
+		define('AES_256_CBC', 'aes-256-cbc');
+		$password = $_GET["cipher"];
 
-	<?php
-	$benutzername = “name”;
-	$_GETmasterpassword = “password”;
-
-	static $encryption_key = openssl_random_pseudo_bytes(32);
-	$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-
-	openssl_encrypt($password, 'aes-256-cbc', $encryptionKey, 0, $iv);
-	$encrypted = $encrypted . ':' . $iv;
-	openssl_decrypt($encryptedData, 'aes-256-cbc', $encryptionKey, 0, 	$initializationVector);
-
-
-
-
-?>
+		echo nl2br("$password\n");
+		//Vorbereitung der Verschlüsselung
+		$encryption_key = openssl_random_pseudo_bytes(32);  //Key wird immer der selbe genutzt. er wird in einer extra php gespeichert und mit Getter geholt
+		$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(AES_256_CBC));
+		//Verschlüsselung
+		$encrypted = openssl_encrypt($password, AES_256_CBC, $encryption_key, 0, $iv);
+		echo nl2br("$encrypted\n");
+		$encrypted = $encrypted . ':' . $iv;
+		//Entschlüsselung
+		$parts = explode(':',$encrypted);
+		$decrypted = openssl_decrypt($parts[0], AES_256_CBC, $encryption_key, 0, $parts[1]);
+		echo nl2br("$decrypted\n");
+	}else{
+		echo "Login Fehlgeschlagen";
+		header("Location: http://localhost/dashboard/Login.php");
+		exit;
+	}
+		 ?>
 	</body>
 </html>
